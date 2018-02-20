5289980a59590439b0546f24d7a66add0c901e8d
supaporn085 <may0936107239@gmail.com>
2018-02-20 12:21:16 +0700

    loginModel.

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -7,13 +7,11 @@ import java.sql.SQLException;
 public class dbConnection {
 
     private static final String SQCONN = "jdbc:sqlite:school.sqlite";
-    public static Connection getConnection(){
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(SQCONN);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
             e.printStackTrace();
         }
 
