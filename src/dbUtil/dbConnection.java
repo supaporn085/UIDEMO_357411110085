2d6a1b319925ca50a2818fc87cc35a18421b0730
supaporn085 <may0936107239@gmail.com>
2018-02-20 10:52:36 +0700

    Initial commit: First Run

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,24 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
+    private static Connection getConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }//getConnection
+
+}//class
